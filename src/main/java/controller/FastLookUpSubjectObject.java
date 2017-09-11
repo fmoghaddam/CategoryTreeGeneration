41ed461b3b42a4c6fa8b679c3237405ba080a7b7
fmoghaddam <f.bakhshandegan@gmail.com>
2017-09-11 17:32:54 +0200

    added comments Fixed bug

--- src/main/java/controller/FastLookUpSubjectObject.java
+++ src/main/java/controller/FastLookUpSubjectObject.java
@@ -9,13 +9,18 @@ import java.util.Set;
 import model.SubjectObject;
 
 public class FastLookUpSubjectObject {
+	/**
+	 * mapping between super category to all its direct sub categories
+	 */
 	private static final Map<String,Set<String>> data = new HashMap<>();
 	
 	public FastLookUpSubjectObject(final List<SubjectObject> sos) {
 		for(SubjectObject so:sos) {
-			final Set<String>  set = data.get(so.getObject());
+			Set<String>  set = data.get(so.getObject());
 			if(set == null) {
-				data.put(so.getObject(), new HashSet<String>());
+				set = new HashSet<String>();
+				set.add(so.getSubject());
+				data.put(so.getObject(), set);
 			}else {
 				set.add(so.getSubject());
 				data.put(so.getObject(), set);
